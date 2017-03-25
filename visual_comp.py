df0f58fcdd67fcf8b18153e8fdeb4aac8216b132
Marcos M. Raimundo <marcosmrai@gmail.com>
2017-03-25 00:03:37 -0300

    Updates

--- visual_comp.py
+++ visual_comp.py
@@ -11,7 +11,7 @@ sys.path+=[__FILE_FOLDER]
 import nonparametric_tests as non
 import igraph
 
-def multitest(threat, names, threshold=0.01, multi_test='skillings_test', multi_posthoc='finner_multitest', imgpath=None, subset=None):
+def multitest(threat, names, threshold=0.01, multi_test='skillings_test', multi_posthoc='finner_multitest', plot=None, subset=None):
     func_test = getattr(non, multi_test)
     func_posthoc = getattr(non, multi_posthoc)
     
@@ -23,16 +23,8 @@ def multitest(threat, names, threshold=0.01, multi_test='skillings_test', multi_
     multiargs = {nam:ran for nam,ran in zip(names, pivots)}
     
     if pvalue<threshold:
+        A = np.zeros((len(names),len(names)))
         comps,_,_,pns = func_posthoc(multiargs)
-        
-        g = igraph.Graph(directed=True)
-        g.add_vertices(len(subset))
-        g.vs["name"] = subset
-        g.vs["label_dist"] = -3
-        g.vs["label"] = g.vs["name"]
-        g.vs["color"] = 'white'
-        g.vs["label_size"] = 25
-        g.layout_circle()
         
         for comp,pn in zip(comps,pns):
             n1,n2 = comp.split(' vs ')
@@ -44,15 +36,25 @@ def multitest(threat, names, threshold=0.01, multi_test='skillings_test', multi_
             i2 = subset.index(n2)
             if pn<threshold:
                 if pivots[i1]<pivots[i2]:
-                    g.add_edges([(i1,i2)])
+                    #print(n1+' is better than '+n2)
+                    A[i1,i2] = 1
                 else:
-                    g.add_edges([(i2,i1)])
-        layout = g.layout_circle()
-        if imgpath==None:
-            igraph.plot(g, layout = layout, margin = 40)
-        else:
-            igraph.plot(g, imgpath, layout = layout, margin = 40)
+                    #print(n2+' is better than '+n1)
+                    A[i2,i1] = 1
+        
+        if plot!=False:
+            g = igraph.Graph(directed=True).Adjacency(A.tolist())
+            g.vs["name"] = subset
+            g.vs["label_dist"] = -3
+            g.vs["label"] = g.vs["name"]
+            g.vs["color"] = 'white'
+            g.vs["label_size"] = 25
+            layout = g.layout_circle()
+            if plot==True:
+                igraph.plot(g, layout = layout, margin = 40)
+            else:
+                igraph.plot(g, plot, layout = layout, margin = 40)            
 
-        return np.array(g.get_adjacency().data)
+        return A, ranking
     else:
         return None
